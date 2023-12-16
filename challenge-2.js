1a1c58268f419aefd6416e4614cfe05d7079b848
SIMON-KANGI <simonmwangikangi@gmail.com>
2023-12-16 11:12:49 +0300

    challenge-2 added

---
+++ challenge-2.js
@@ -0,0 +1,21 @@
+
+const prompt=require('prompt-sync')({sigint:true}) //initialized prompt
+// a function to hold the program
+const speedDetector= function(){
+let speed=prompt("The speed is:")
+let maxSpeed=70;// initial max-speed
+let points=0 //initial points
+if(speed<=70){
+   return `OK  and demerit points is ${points}`
+}else if(speed>70){
+    let extraSpeed= speed-maxSpeed //calculate the amount of speed above the limit
+    let pointsGotten=Math.floor(extraSpeed/5); //divide by five then covert the answer to the nearest integer
+    points+=pointsGotten //calculates the number of points
+    if(points>=12){
+        return "You points are too high. License suspended!" // if points are equal to 12 or higher
+    }else{
+        return `your points are ${points}`
+    }
+}
+}
+console.log(speedDetector())
